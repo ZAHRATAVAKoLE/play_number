47f69747102f405a6086ff55b21bd89270f51985
ZahraTavakoli <100377681+ZAHRATAVAKoLE@users.noreply.github.com>
2022-04-13 15:23:11 +0430

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Random r;
+        int computer_number;
+        public Form1()
+        {
+            InitializeComponent();
+            r = new Random();
+            computer_number = r.Next(0, 100);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                int user_number = Convert.ToInt32(textBox1.Text);
+                if (user_number == computer_number)
+                {
+
+                    label1.Text=("you win");
+                }
+                else if (user_number < computer_number)
+                {
+                    label1.Text=("go up");
+                }
+                else if (user_number > computer_number)
+                {
+                    label1.Text=("go down");
+
+                }
+                textBox1.Text = " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Run(new Form());
+        }
+    }
+}
